e019fb59ad6af541f544b9a61645bff7970ba739
DanielMilanez <daniel.augusto.milanez@gmail.com>
2024-09-23 19:41:10 -0300

    atividades

---
+++ JS/Exercicios/ex02.js
@@ -0,0 +1,4 @@
+const ValorConta = Number(prompt("Informe o valor da conta: "));
+const NumClientes = Number(prompt("Informe a quantidade de clientes para dividir a conta: "));
+
+alert(`O valor pago por cliente será de: R$${(ValorConta / NumClientes).toFixed(2)}`);
