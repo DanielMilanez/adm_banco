454b427e3391daa2722e5a121ddaac604ed1895e
DanielMilanez <daniel.augusto.milanez@gmail.com>
2024-10-03 21:37:06 -0300

    subindo para o github pages

--- JS/code07.js
+++ JS/code07.js
@@ -7,13 +7,11 @@ frm.addEventListener("submit", (e)=>{
     const mascluno = frm.inMasculino.checked;
     const altura = Number(frm.inAltura.value);
 
-    let peso;
-
-    if(mascluno){
-        peso = 22 * Math.pow(altura, 2);
-    }else{
-        peso = 21 * Math.pow(altura, 2);
-    }
+    let peso = mascluno ? Math.pow(altura, 2) * 22 : Math.pow(altura, 2 * 21);
 
     resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(2)}kg`;
 });
+
+function limpar(){
+    document.querySelector("h3").innerText = "";
+}
